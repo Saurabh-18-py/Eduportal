71fb0a65268e0a365c910d02fe7a6db5a0208c9e
Saurabh <-puzzyslayer1800@gmail.com>
2026-08-28 09:23:14 +0530

    Remove duplicate nested project files

--- eduportal/accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import StudentProfile
-
-
-@admin.register(StudentProfile)
-class StudentProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'class_level', 'created_at')
-    list_filter = ('class_level',)
