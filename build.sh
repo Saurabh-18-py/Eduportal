fd38af919a2f37f5b32e52c66a7769b4375a43cc
Saurabh <-puzzyslayer1800@gmail.com>
2026-08-29 11:42:27 +0530

    Clear stale staticfiles cache before collectstatic

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+rm -rf staticfiles
+
+python manage.py collectstatic --no-input --clear
 python manage.py migrate
